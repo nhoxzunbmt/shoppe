e2d6fc0b349994b7d93efe7ef4e485283a2b3758
thanhloi <nhoxzunbmt@gmail.com>
2018-06-20 18:15:52 +0700

    u

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import Person from './Person/Person'
 
 class App extends Component {
@@ -18,17 +19,32 @@ class App extends Component {
     title_page : 'List study'
   }
 
-  switchClick = () => {
+  switchClick = (name) => {
     console.log('Was clicked')
     this.setState({
       persons : [
           {
-            name : 'Max' ,
+            name : name ,
             age : 1993
           },
           {
             name : 'Loi Tran',
             age : 2018
+          }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons : [
+          {
+            name : event.target.value,
+            age : 1993
+          },
+          {
+            name : event.target.value,
+            age : 1993
           }
       ]
     })
@@ -40,11 +56,12 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Welcome to Shoppe</h1>
         </header>
-        <button onClick={this.switchClick}>Click</button>
+        <button onClick={() => this.switchClick('Love12')}>Click</button>
      
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>I'm Dev</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>I'm Account</Person>
+        <Person changed={this.nameChangedHandler} name={this.state.persons[0].name} age={this.state.persons[0].age}>I'm Dev</Person>
+        
+        <Person changed={this.nameChangedHandler} name={this.state.persons[1].name} age={this.state.persons[1].age}>I'm Account</Person>
 
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import './Person.css';
+
 const full_name = 'Tran Thanh Loi'
 const person = (props) => {
-    return <p>
+    return <p class="Person">
                 <span> 
                 person {props.name} - age : {props.age} 
                 </span>
+                <input type="text" onChange={props.changed} defaultValue={props.value}/>
                 <br/>
                 {props.children}
             </p>
